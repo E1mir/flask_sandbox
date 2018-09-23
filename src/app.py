61deb436bc89c182a57d0f769a597aa87a646689
E1mir <kryternext@gmail.com>
2018-09-23 22:15:38 +0400

    Flask routing

--- src/app.py
+++ src/app.py
@@ -4,8 +4,28 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return '<h1>Hey there! I\'m using a Flask!</h1>'
+
+
+@app.route('/home')
+def home():
+    return "<h1>You are on the home page!</h1>"
+
+
+@app.route('/home/<string:place>')
+def home_place(place):
+    return '<h1>You are on the ' + place + '</h1>'
+
+
+@app.route('/some_post', methods=['POST'])
+def post_request():
+    return 'Hello from POST!'
+
+
+@app.route('/get_post', methods=['GET', 'POST'])
+def get_post_page():
+    return "It works"
 
 
 if __name__ == '__main__':
