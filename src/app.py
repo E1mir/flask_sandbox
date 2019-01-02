f7fad1f77a05f4a8cfe9f4eaa80ea1feef246c89
Elmir Ismayilzade <kryternext@smartup.school>
2019-01-02 17:23:12 +0400

    Project refactoring: remove unused code examples.

--- src/app.py
+++ src/app.py
@@ -8,47 +8,34 @@ from models import Comment
 @app.route('/')
 def index():
     comments = Comment.query.all()
-    return render_template('project/index.html', title='Index page', page='index', comments=comments)
+    # another way to create and use context
+    context = {
+        'title': 'Index page',
+        'page': 'index',
+        'comments': comments
+    }
+    return render_template(
+        'project/index.html',
+        **context
+    )
 
 
 @app.route('/add_comment')
 def add_comment():
-    return render_template('project/add_comment_form.html', title='Add comment page', page='add_comment')
-
-
-@app.route('/example')
-def example():
-    context = {
-        'hello': 'Hello from FLASK.PY'
-    }
-    return render_template('example/example.html', context=context)
-
-
-@app.route('/home')
-def home():
-    return "<h1>You are on the home page!</h1>"
-
-
-@app.route('/home/<string:place>')
-def home_place(place):
-    return f'<h1>You are on the {place}!</h1>'
-
-
-@app.route('/some_post', methods=['POST'])
-def post_request():
-    return 'Hello from POST!'
-
-
-@app.route('/get_post', methods=['GET', 'POST'])
-def get_post_page():
-    return "It works"
+    return render_template(
+        'project/add_comment_form.html',
+        title='Add comment page',
+        page='add_comment'
+    )
 
 
 @app.route('/process', methods=['POST'])
 def process():
     name = request.form['name']
     comment = request.form['comment']
+    # create a signature
     signature = Comment(name=name, comment=comment)
+    # add it into database session then save it.
     db_session.add(signature)
     db_session.commit()
     return redirect(url_for('index'))
